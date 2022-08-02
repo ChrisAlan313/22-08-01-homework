8075982861fd7783b5aaa711b3f723ca0c89c269
Chris Booty <chrisalan313@gmail.com>
2022-08-01 21:53:15 -0400

    Rename and pass tests

--- optional-chaining.js
+++
@@ -1,8 +0,0 @@
-
-const notOptionalChain = (test_object, thing_name) => {
-  return test_object.thing.name === thing_name;
-}
-
-const optional_chain = function() {}
-
-module.exports = notOptionalChain;
